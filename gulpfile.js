20f58f0b0ff14853928d13c31e320b192de46bbc
L-Wiss <wassimbenameur@gmail.com>
2016-02-03 18:06:57 +0100

    adding harmony

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 'use strict';
 
 var gulp = require('gulp'),
-	nodemon = require('gulp-nodemon');
+	nodemon = require('gulp-nodemon'),
+	babel = require('gulp-babel');
 
 
 
-gulp.task('default',function () {
-	console.log('launching task');
+gulp.task('default', function () {
 	nodemon({
 		script:'./app.js',
 		ext:'js',
+	    execMap: {
+	      js: "node --harmony --use_strict"
+	    },
 		ignore:'node_modules/**'
 	})
+});
+
+gulp.task('build.js', function() {
+	return gulp.src('./src/js/flattify.js')
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('build'));
 })
+
