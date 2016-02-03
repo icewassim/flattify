6fd0b4dfc63176890c3f71718d9beebca2afe249
L-Wiss <wassimbenameur@gmail.com>
2016-02-03 18:12:38 +0100

    setting const

--- src/js/flattify.js
+++ src/js/flattify.js
@@ -1,3 +1,5 @@
+const RGBA = 4;
+
 class ImgCanvas {
 
 	constructor(imgData) {
@@ -14,12 +16,12 @@ class ImgCanvas {
 	imgDataToMatrix() {
 	    for (let i = 0; i < this.height; i++) {
 	      let lines = [];
-	       for (let j = 0; j < this.width*4 ;j = j+4) {
+	       for (let j = 0; j < this.width*RGBA ;j = j+RGBA) {
 	          let pixel = {};
-	          pixel.r = imgData.data[j+(i*this.width*4)];
-	          pixel.g = imgData.data[j+1+(i*this.width*4)];
-	          pixel.b = imgData.data[j+2+(i*this.width*4)];
-	          pixel.a = imgData.data[j+3+(i*this.width*4)];
+	          pixel.r = imgData.data[j+(i*this.width*RGBA)];
+	          pixel.g = imgData.data[j+1+(i*this.width*RGBA)];
+	          pixel.b = imgData.data[j+2+(i*this.width*RGBA)];
+	          pixel.a = imgData.data[j+3+(i*this.width*RGBA)];
 	          lines.push(pixel);
 	        }
 	        imgMatrix.push(lines);
