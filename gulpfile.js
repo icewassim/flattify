4802bc59a3554dc255fcc47f69e1ed740067ab3e
icewassim <wassimbenameur@gmail.com>
2016-02-04 00:58:17 +0100

    beautify

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,50 @@
 
 var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
-	nodemon = require('gulp-nodemon'),
-	babelify = require('babelify'),
-	source = require('vinyl-source-stream'),
-	fs = require('fs'),
-	runSequence = require('run-sequence'),
-	del = require('del'),
-	browserify = require('browserify');
+  nodemon = require('gulp-nodemon'),
+  babelify = require('babelify'),
+  source = require('vinyl-source-stream'),
+  fs = require('fs'),
+  runSequence = require('run-sequence'),
+  del = require('del'),
+  browserify = require('browserify');
 
-gulp.task('default', function () {
-	return nodemon({
-		script:'./src/app.js',
-		ext:'js',
-	    execMap: {
-	      js: "node --harmony --use_strict"
-	    },
-		ignore:'node_modules/**'
-	})
+gulp.task('default', function() {
+  return nodemon({
+    script: './src/app.js',
+    ext: 'js',
+    execMap: {
+      js: "node --harmony --use_strict"
+    },
+    ignore: 'node_modules/**'
+  })
 });
 
-gulp.task('clean', function () {
-    return  del(['build/**/*']);
+gulp.task('clean', function() {
+  return del(['build/**/*']);
 });
 
 gulp.task('js:lint', function() {
-    return gulp.src('./src/**/*.js')
-        .pipe(jshint({ esnext: true }))
-        .pipe(jshint.reporter('default'));
+  return gulp.src('./src/**/*.js')
+    .pipe(jshint({
+      esnext: true
+    }))
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('build', function() {
-	runSequence('clean', 'js:compile');
+  runSequence('clean', 'js:compile');
 })
 
 gulp.task('js:compile', ['js:lint'], function() {
-	    return browserify()
-  			.transform("babelify",
-					{presets: ["es2015"]})
-				.require('./src/js/flattify.js', {
-							entry: true
-				})
-				.bundle()
-				.pipe(source('flattify.js'))
-  			.pipe(gulp.dest('./build/'));
+  return browserify()
+    .transform("babelify", {
+      presets: ["es2015"]
+    })
+    .require('./src/js/flattify.js', {
+      entry: true
+    })
+    .bundle()
+    .pipe(source('flattify.js'))
+    .pipe(gulp.dest('./build/'));
 });
--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
 const express = require('express'),
-      consolidate = require('consolidate'),
-      path = require('path'),
-      app = express();
+  consolidate = require('consolidate'),
+  path = require('path'),
+  app = express();
 
-let port =  process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.engine('html', consolidate.handlebars);
-app.use('/pics', express.static(__dirname+'/pics'));
-app.use('/js', express.static(__dirname+'/../build'));
+app.use('/pics', express.static(__dirname + '/pics'));
+app.use('/js', express.static(__dirname + '/../build'));
 app.set('view engine', 'html');
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 
-app.listen(port,function (err) {
-  if(err) {
-    console.error('Failure while openinig port ',port);
+app.listen(port, function(err) {
+  if (err) {
+    console.error('Failure while openinig port ', port);
   }
-  console.log('listenting to port ',port);
+  console.log('listenting to port ', port);
 });
 
-app.get('/',function(req, res) {
-  res.status(200).render('index',{hello:"lol"});
+app.get('/', function(req, res) {
+  res.status(200).render('index', {
+    hello: "lol"
+  });
 });
