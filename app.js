211badb69238b28a7ecd02be3b1df5acb7cc798f
L-Wiss <wassimbenameur@gmail.com>
2016-02-03 18:07:15 +0100

    using strict mode

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-var express = require('express'),
-    path = require('path'),
-    app = express();
+const express = require('express'),
+      path = require('path'),
+      app = express();
 
-app.listen(3000,function (err) {
+let port =  process.env.PORT || 3000;
+
+app.listen(port,function (err) {
   if(err) {
-    console.log('error openinig port ');
+    console.error('Failure while openinig port ',port);
   }
-  console.log('listenting to port 3000');
+  console.log('listenting to port ',port);
 });
 
+
+//TODO routes, controllers ,view engines
 app.get('/source.png',function (rq,res) {
-  console.log(path.join(__dirname+'/public/pics/soundcloud.png'));
-  res.status(200).sendFile(path.join(__dirname+'/public/pics/soundcloud.png'));
+  res.status(200).sendFile(path.join(__dirname+'/src/pics/soundcloud.png'));
 })
 
 app.get('/',function(req, res) {
