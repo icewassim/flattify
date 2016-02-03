f06738eaf38dddb687339e5d86b4ffaef5165ae3
icewassim <wassimbenameur@gmail.com>
2016-02-04 00:40:23 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 'use strict';
 
 var gulp = require('gulp'),
+  jshint = require('gulp-jshint'),
 	nodemon = require('gulp-nodemon'),
-	babel = require('gulp-babel');
+	babelify = require('babelify'),
+	source = require('vinyl-source-stream'),
+	fs = require('fs'),
+	runSequence = require('run-sequence'),
+	del = require('del'),
+	browserify = require('browserify');
 
 gulp.task('default', function () {
-	nodemon({
-		script:'./app.js',
+	return nodemon({
+		script:'./src/app.js',
 		ext:'js',
 	    execMap: {
 	      js: "node --harmony --use_strict"
@@ -15,10 +21,34 @@ gulp.task('default', function () {
 	})
 });
 
-gulp.task('build.js', function() {
-	return gulp.src('./src/js/*')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest('build'));
+gulp.task('clean', function () {
+    return  del(['build/**/*']);
+});
+
+gulp.task('js:lint', function() {
+    return gulp.src('./src/**/*.js')
+        .pipe(jshint({ esnext: true }))
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('build', function() {
+	runSequence('clean', 'js:compile');
+})
+
+gulp.task('js:compile', ['js:lint'], function() {
+	    return browserify()
+  			.transform("babelify",
+					{presets: ["es2015"]})
+				.require('./src/js/flattify.js', {
+							entry: true
+				})
+				.bundle()
+				.pipe(source('flattify.js'))
+  			.pipe(gulp.dest('./build/'));
+	//
+	// return gulp.src('./src/js/*')
+	// 	.pipe(babel({
+	// 		presets: ['es2015']
+	// 	}))
+	// 	.pipe(gulp.dest('build'));
 });
