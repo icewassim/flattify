3d5ac4d89d28db6db058434c90794fb6b358aebf
icewassim <wassimbenameur@gmail.com>
2016-02-04 23:40:44 +0100

    Matrix to img data fix

--- src/js/ImgCanvas.js
+++ src/js/ImgCanvas.js
@@ -1,4 +1,4 @@
-const RGBA = 4;
+const RGBA_COUNT = 4;
 class ImgCanvas {
 
   constructor(imgData) {
@@ -14,18 +14,32 @@ class ImgCanvas {
   }
 
   get imgData() {
+    this.matrixToImgData();
     return this._imgData;
+  }
+
+  matrixToImgData() {
+    let RGBIndex = 0;
+    for (let i = 0; i < this.imgMatrix.length; i++) {
+      for (let j = 0; j < this.imgMatrix[i].length; j++) {
+        this._imgData.data[RGBIndex] = this.imgMatrix[i][j].r;
+        this._imgData.data[RGBIndex + 1] = this.imgMatrix[i][j].g;
+        this._imgData.data[RGBIndex + 2] = this.imgMatrix[i][j].b;
+        this._imgData.data[RGBIndex + 3] = this.imgMatrix[i][j].a;
+        RGBIndex = RGBIndex + RGBA_COUNT;
+      }
+    }
   }
 
   imgDataToMatrix() {
     for (let i = 0; i < this.height; i++) {
       let lines = [];
-      for (let j = 0; j < this.width * RGBA; j = j + RGBA) {
+      for (let j = 0; j < this.width * RGBA_COUNT; j = j + RGBA_COUNT) {
         let pixel = {};
-        pixel.r = this._imgData.data[j + (i * this.width * RGBA)];
-        pixel.g = this._imgData.data[j + 1 + (i * this.width * RGBA)];
-        pixel.b = this._imgData.data[j + 2 + (i * this.width * RGBA)];
-        pixel.a = this._imgData.data[j + 3 + (i * this.width * RGBA)];
+        pixel.r = this._imgData.data[j + (i * this.width * RGBA_COUNT)];
+        pixel.g = this._imgData.data[j + 1 + (i * this.width * RGBA_COUNT)];
+        pixel.b = this._imgData.data[j + 2 + (i * this.width * RGBA_COUNT)];
+        pixel.a = this._imgData.data[j + 3 + (i * this.width * RGBA_COUNT)];
         lines.push(pixel);
       }
       this.imgMatrix.push(lines);
@@ -34,25 +48,25 @@ class ImgCanvas {
 
   shadowfy() {
     for (let i = 0; i < this.imgMatrix.length; i++) {
-      for (let j= 0; j < this.imgMatrix[i].length; j++) {
-        if(this.imgMatrix[i][j].background !== true) {
-          for (let decrease = 1; decrease + i < this.imgMatrix.length && decrease + j < this.imgMatrix.length ; decrease++) {
-            if(this.imgMatrix[i+decrease][j+decrease].background === true) {
-                this.imgMatrix[i + decrease][j+decrease].r = 0;
-                this.imgMatrix[i + decrease][j+decrease].g = 0;
-                this.imgMatrix[i + decrease][j+decrease].b = 0;
-                this.imgMatrix[i + decrease][j+decrease].a = 70;
-              }
+      for (let j = 0; j < this.imgMatrix[i].length; j++) {
+        if (this.imgMatrix[i][j].background !== true) {
+          for (let decrease = 1; decrease + i < this.imgMatrix.length && decrease + j < this.imgMatrix.length; decrease++) {
+            if (this.imgMatrix[i + decrease][j + decrease].background === true) {
+              this.imgMatrix[i + decrease][j + decrease].r = 0;
+              this.imgMatrix[i + decrease][j + decrease].g = 0;
+              this.imgMatrix[i + decrease][j + decrease].b = 0;
+              this.imgMatrix[i + decrease][j + decrease].a = 70;
             }
           }
         }
       }
+    }
   }
 
   halfMaterial() {
     for (let i = 0; i < imgMatrix.length; i++) {
-      for (let j = imgMatrix[i].length/2; j < imgMatrix[i].length; j++) {
-        imgMatrix[i][j].g  = imgMatrix[i][j].g - 20;
+      for (let j = imgMatrix[i].length / 2; j < imgMatrix[i].length; j++) {
+        imgMatrix[i][j].g = imgMatrix[i][j].g - 20;
       }
     }
   }
--- src/js/flattify.js
+++ src/js/flattify.js
@@ -4,32 +4,17 @@ let canvas = document.getElementById('canvas');
 let outputCanvas = document.getElementById('canvasOutput');
 let ctx = canvas.getContext('2d');
 
-console.log("flattify");
 function draw() {
-  ctx.drawImage(document.getElementById('source'),0,0);
+  ctx.drawImage(document.getElementById('source'), 0, 0);
 }
-  
-window.onload = function() {
-    draw();
-    let height = 500;
-    let width = 500;
-    let imgData = ctx.getImageData(0,0,height,width);
-    let iconCanvas = new ImgCanvas(imgData);
-    iconCanvas.init();
-    iconCanvas.shadowfy();
-    console.log(iconCanvas.imgData);
-    ctx.putImageData(iconCanvas.imgData,0,0);
-  //   //  var ImgCanvas = new ImgCanvas(ImgCanvas);
 
-  //   var imgMatrixg = imgDataToMatrix(imgData,height,width);
-  //   var shared = markBackground({r:0},{r:0},imgMatrixg);
-  //   shared = shaderdMatrix( markBackground({r:200},{r:255},imgMatrixg,24) );
-  //   //var halfShadowed = halfShadow(imgDataToMatrix(imgData,height,width));
-  //   var newImageData = MatrixToImgData(shared,imgData);
-  // /*
-  //     0 -> 2000 (4 * width)
-  //     2000 -> 4000 (4*with * 2)
-  //     4000 -> 6000 (4*with * 3)
-  // */
-    console.log("hello3",imgData);
- };
+window.onload = function() {
+  draw();
+  let height = 500;
+  let width = 500;
+  let imgData = ctx.getImageData(0, 0, height, width);
+  let iconCanvas = new ImgCanvas(imgData);
+  iconCanvas.init();
+  iconCanvas.shadowfy();
+  ctx.putImageData(iconCanvas.imgData, 0, 0);
+};
