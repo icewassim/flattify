1a2f22bde5ac9b6f2aec15c2dd2d9d8a5f2dc540
icewassim <wassimbenameur@gmail.com>
2016-02-07 11:28:56 +0100

    update desc

--- tests/ImgCanvasTests.js
+++ tests/ImgCanvasTests.js
@@ -1,49 +1,50 @@
  import ImgCanvas from '../src/js/ImgCanvas';
  import * as testLib from './testLib';
 
-const fs = require("fs"),
-  assert = require('assert');
+ const fs = require("fs"),
+   assert = require('assert');
 
-describe('ImgCanvas test', function() {
-  let imgData = {};
-  before(function(done) {
-    fs.readFile(__dirname + '/ImgDataTwitter.json', function(err, content) {
-      if (err) {
-        console.error("cant read file ", err);
-      }
-      imgData = JSON.parse(content.toString());
-      ImgCanvas.prototype.getAttributes = testLib.getPrivAttr;
-      done();
-    });
-  });
+ describe('ImgCanvas test', function() {
+   let imgData = {};
+   before(function(done) {
+     fs.readFile(__dirname + '/ImgDataTwitter.json', function(err, content) {
+       if (err) {
+         console.error("cant read file ", err);
+       }
+       imgData = JSON.parse(content.toString());
+       ImgCanvas.prototype.getAttributes = testLib.getPrivAttr;
+       done();
+     });
+   });
 
-  it("should return an iconCanvas Object", function() {
-    let iconCanvas = new ImgCanvas(imgData);
-    assert.equal(testLib.isEmptyObj(iconCanvas), false);
+   it("should return an iconCanvas Object", function() {
+     let iconCanvas = new ImgCanvas(imgData);
+     assert.equal(testLib.isEmptyObj(iconCanvas), false);
 
-  });
-  it('should have valid dimensions ', function() {
-    let iconCanvas = new ImgCanvas(imgData);
-    assert.equal(iconCanvas.getAttributes().height,imgData.height);
-    assert.equal(iconCanvas.getAttributes().width,imgData.width);
-  });
+   });
 
-  it('should return an empty object if imgData is missing', function () {
-    let iconCanvas = new ImgCanvas();
-    assert.equal(testLib.isEmptyObj(iconCanvas),true);
-  });
+   it('should have valid dimensions ', function() {
+     let iconCanvas = new ImgCanvas(imgData);
+     assert.equal(iconCanvas.getAttributes().height, imgData.height);
+     assert.equal(iconCanvas.getAttributes().width, imgData.width);
+   });
 
-  it('should transform imageData to a Matrix with valid dimensions', function() {
-    let iconCanvas,
-        imgMatrix;
+   it('should return an empty object if imgData is missing', function() {
+     let iconCanvas = new ImgCanvas();
+     assert.equal(testLib.isEmptyObj(iconCanvas), true);
+   });
 
-    iconCanvas = new ImgCanvas(imgData);
-    iconCanvas.imgDataToMatrix();
-    imgMatrix = iconCanvas.getAttributes().imgMatrix;
+   it('should transform imageData to a Matrix with valid dimensions', function() {
+     let iconCanvas,
+       imgMatrix;
 
-    assert.equal(imgMatrix.constructor, Array);
-    assert.equal(imgMatrix.length,imgData.width);
-    assert.equal(imgMatrix[1].length,imgData.height);
-  });
+     iconCanvas = new ImgCanvas(imgData);
+     iconCanvas.imgDataToMatrix();
+     imgMatrix = iconCanvas.getAttributes().imgMatrix;
 
-});
+     assert.equal(imgMatrix.constructor, Array);
+     assert.equal(imgMatrix.length, imgData.width);
+     assert.equal(imgMatrix[1].length, imgData.height);
+   });
+
+ });
