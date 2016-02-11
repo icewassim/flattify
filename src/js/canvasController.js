7d396870481b5de0a904ba2f1940060ea6d93b54
icewassim <wassimbenameur@gmail.com>
2016-02-11 23:52:10 +0100

    iconcanvas fixed

--- src/js/canvasController.js
+++ src/js/canvasController.js
@@ -1,25 +1,25 @@
 import ImgCanvas from './ImgCanvas';
 
 class canvasController {
-  constructor(canvasId, iconId,options) {
+  constructor(canvasId, iconId, options) {
     this.canvas = document.getElementById(canvasId);
     this.icon = document.getElementById(iconId);
-    //default options
 
+    //default options
     this.options = {
-      margin : 100,
-      backgroundColor : {
-              r: 0,
-              g: 184,
-              b: 255,
-              a: 255
+      margin: 100,
+      backgroundColor: {
+        r: 0,
+        g: 184,
+        b: 255,
+        a: 255
       },
-      shape : "rect"
+      shape: "rect"
     };
 
-    if(typeof(options) === "object") {
-      for(let i in options) {
-        if(options.hasOwnProperty(i))
+    if (typeof(options) === "object") {
+      for (let i in options) {
+        if (options.hasOwnProperty(i))
           this.options[i] = options[i];
       }
     }
@@ -27,42 +27,38 @@ class canvasController {
 
   init() {
     let imgData;
-    console.log(this.options)
+
     this.width = parseInt(this.icon.width);
     this.height = parseInt(this.icon.height);
     this.ctx = this.canvas.getContext("2d");
+    this.canvas.height = this.icon.height + this.options.margin;
+    this.canvas.width = this.icon.width + this.options.margin;
+    this.initBackground();
+    this.ctx.drawImage(this.icon, this.options.margin / 2, this.options.margin / 2);
     imgData = this.ctx.getImageData(0, 0, this.height + this.options.margin, this.width + this.options.margin);
     this.iconCanvas = new ImgCanvas(imgData);
-    //this.initBackground();
-    //this.iconCanvas.init(this.options.backgroundColor);
-    //this.iconCanvas.shadowfy();
-    this.iconCanvas.init({
-      r: 0,
-      g: 184,
-      b: 255,
-      a: 255
-    });
-    console.log(this.iconCanvas.imgData);
+    this.iconCanvas.init(this.options.backgroundColor);
+    this.iconCanvas.shadowfy();
     this.ctx.putImageData(this.iconCanvas.imgData, 0, 0);
   }
 
   initBackground() {
     let centerX = this.canvas.width / 2,
-        centerY = this.canvas.height / 2,
-        radius = Math.max(this.icon.height, this.icon.width) - 90;
+      centerY = this.canvas.height / 2,
+      radius = Math.max(this.icon.height, this.icon.width) - 90;
 
     this.ctx.beginPath();
-    if (this.shape === "rect") {
+    if (this.options.shape === "rect") {
       this.ctx.rect(0, 0, this.canvas.height, this.canvas.width, false);
-    } else if (this.shape === "circle") {
+    } else if (this.options.shape === "circle") {
       this.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     }
 
-    this.ctx.fillStyle = 'rgba(0, 184, 255, 1)';
-    /*this.ctx.fillStyle = "rgba("+this.options.backgroundColor.r+","+
-                                  this.options.backgroundColor.g+","+
-                                  this.options.backgroundColor.b+","+
-                                  "1,);";*/
+    this.ctx.fillStyle = "rgba(" + this.options.backgroundColor.r + "," +
+      this.options.backgroundColor.g + "," +
+      this.options.backgroundColor.b + "," +
+      "1)";
+
     this.ctx.fill();
   }
 
