f0eb4279a6c683b6fb842fa6172d01998b3ddb12
L-Wiss <wassimbenameur@gmail.com>
2016-02-12 16:15:18 +0100

    update canvas class

--- src/js/canvasController.js
+++ src/js/canvasController.js
@@ -14,7 +14,8 @@ class canvasController {
         b: 255,
         a: 255
       },
-      shape: "rect"
+      shape: "rect",
+      isShadowfied :true
     };
 
     if (typeof(options) === "object") {
@@ -38,7 +39,8 @@ class canvasController {
     imgData = this.ctx.getImageData(0, 0, this.height + this.options.margin, this.width + this.options.margin);
     this.iconCanvas = new ImgCanvas(imgData);
     this.iconCanvas.init(this.options.backgroundColor);
-    this.iconCanvas.shadowfy();
+    if(this.options.isShadowfied === true)
+      this.iconCanvas.shadowfy();
     this.ctx.putImageData(this.iconCanvas.imgData, 0, 0);
   }
 
@@ -54,7 +56,8 @@ class canvasController {
       this.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     }
 
-    this.ctx.fillStyle = "rgba(" + this.options.backgroundColor.r + "," +
+    this.ctx.fillStyle = "rgba(" + 
+      this.options.backgroundColor.r + "," +
       this.options.backgroundColor.g + "," +
       this.options.backgroundColor.b + "," +
       "1)";
@@ -62,9 +65,64 @@ class canvasController {
     this.ctx.fill();
   }
 
-  setBackground(shape) {
-    this.shape = shape;
-    this.reloadCanvas();
+  setBackgroundShape(shape) {
+    if(typeof(shape) === "string" && shape.length > 0)
+      this.options.shape = shape;
+  }
+
+
+  setBackgroundColor(color) {
+    let pixel = this.rgbaToPixel(color);
+    
+    console.log(pixel);
+    if(this.isValidPixel(pixel) === false) {
+      console.error("invalid color");
+      return false;
+    }
+
+    console.log(pixel);
+    this.options.backgroundColor = pixel;
+  }
+
+  reloadCanvas() {
+    let imgData;
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.initBackground();
+    this.ctx.drawImage(this.icon, this.options.margin / 2, this.options.margin / 2);
+    imgData = this.ctx.getImageData(0, 0, this.height + this.options.margin, this.width + this.options.margin);
+    this.iconCanvas = new ImgCanvas(imgData);
+    this.iconCanvas.init(this.options.backgroundColor);
+    if(this.options.isShadowfied === true)
+      this.iconCanvas.shadowfy();
+    this.ctx.putImageData(this.iconCanvas.imgData, 0, 0);
+  }
+
+  rgbaToPixel(rgbaColor) {
+    let rgbaTab = rgbaColor.replace("rgba(","").replace(")","").split(",");
+    return {
+      r: parseInt(rgbaTab[0]),
+      g: parseInt(rgbaTab[1]),
+      b: parseInt(rgbaTab[2]),
+      a: 255
+    };
+  }
+
+  isValidPixel(pixel) {
+    let count = 0;
+    if (typeof(pixel) !== "object")
+      return false;
+
+    for (let i in pixel) {
+      count ++;
+      if(typeof(pixel[i]) !== "number"  || pixel[i] > 255 || pixel[i] < 0)
+        return false;
+    }
+
+    if(count != 4)
+      return false;
+
+    return true;
+
   }
 
   shadowfy() {
--- src/js/flattify.js
+++ src/js/flattify.js
@@ -1,104 +1,25 @@
-//import ImgCanvas from './ImgCanvas';
 import CanvasController from './canvasController';
-
-function initCanvas(canvas, img, margin, backgroundColor,shape) {
-  'use strict';
-  let ctx = canvas.getContext('2d');
-  canvas.height = img.height + margin;
-  canvas.width = img.width + margin;
-  initBackground(canvas, img, backgroundColor, ctx, shape);
-  ctx.drawImage(img, margin / 2, margin / 2);
-  return ctx;
-}
-
-function initBackground(canvas, img, backgroundColor, ctx, shape) {
-  'use strict';
-  let centerX = canvas.width / 2,
-    centerY = canvas.height / 2,
-    radius = Math.max(img.height, img.width) - 90;
-
-  ctx.beginPath();
-  if (shape === "rect") {
-    ctx.rect(0, 0, canvas.height, canvas.width, false);
-  } else if (shape === "circle") {
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  }
-  ctx.fillStyle = backgroundColor;
-  ctx.fill();
-}
 
 window.onload = function() {
   'use strict';
-  let img = document.getElementById("source"),
-    canvas = document.getElementById('canvas'),
-    width = parseInt(img.width),
-    height = parseInt(img.height),
-    imgData,
-    iconCanvas,
-    margin = 100,
-    ctx;
 
-
-  var canvasController = new CanvasController("canvas","source");
+  let canvasController = new CanvasController("canvas","source");
   canvasController.init();
+
+  document.getElementById("rect").onclick = function() {
+    canvasController.setBackgroundShape("rect");
+    canvasController.reloadCanvas();
   };
-/*  
+
   document.getElementById("circular").onclick = function() {
-    ctx = initCanvas(canvas, img, margin, 'rgba(0, 184, 255, 1)','circle');
-    imgData = ctx.getImageData(0, 0, height + margin, width + margin);
-    iconCanvas = new ImgCanvas(imgData);
-    iconCanvas.init({
-      r: 0,
-      g: 184,
-      b: 255,
-      a: 255
-    });
-    iconCanvas.shadowfy();
-    ctx.putImageData(iconCanvas.imgData, 0, 0);
+    canvasController.setBackgroundShape("circle");
+    canvasController.reloadCanvas();
   };
 
   document.getElementById('backgroundColor').onclick = function() {
-    var color = document.getElementById("backgroundColorValue").value;
-    var rgbaTab = color.replace("rgba(","").replace(")","").split(",");
-    console.log(rgbaTab);
-
-    ctx = initCanvas(canvas, img, margin, color,'circle');
-    imgData = ctx.getImageData(0, 0, height + margin, width + margin);
-    iconCanvas = new ImgCanvas(imgData);
-    iconCanvas.init({
-      r: rgbaTab[0],
-      g: rgbaTab[1],
-      b: rgbaTab[2],
-      a: 255
-    });
-    iconCanvas.shadowfy();
-    ctx.putImageData(iconCanvas.imgData, 0, 0);
+    let color = document.getElementById("backgroundColorValue").value;
+    canvasController.setBackgroundColor(color);
+    canvasController.reloadCanvas();
   };
 
-  document.getElementById("rect").onclick = function() {
-    ctx = initCanvas(canvas, img, margin, 'rgba(0, 184, 255, 1)','rect');
-    imgData = ctx.getImageData(0, 0, height + margin, width + margin);
-    iconCanvas = new ImgCanvas(imgData);
-    iconCanvas.init({
-      r: 0,
-      g: 184,
-      b: 255,
-      a: 255
-    });
-    iconCanvas.shadowfy();
-    ctx.putImageData(iconCanvas.imgData, 0, 0);
   };
-
-  ctx = initCanvas(canvas, img, margin, 'rgba(0, 184, 255, 1)','rect');
-  imgData = ctx.getImageData(0, 0, height + margin, width + margin);
-  iconCanvas = new ImgCanvas(imgData);
-  iconCanvas.init({
-    r: 0,
-    g: 184,
-    b: 255,
-    a: 255
-  });
-  iconCanvas.shadowfy();
-  ctx.putImageData(iconCanvas.imgData, 0, 0);
-};
-*/
